97ee6151ca39b6a3e2da46dcf987826cba93c24f
root <root@localhost.localdomain>
2017-03-07 01:28:54 -0600

    import header files

--- capter2_LLCprogramming/ltest.c
+++ capter2_LLCprogramming/ltest.c
@@ -1,2 +1,14 @@
 // ltest.c link layer test.
 
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+
+#include<sys/ioctl.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+
+//if you want to handle link layer packet, you will need follow headers
+#include<linux/if.h>
+#include<netpacket/packet.h>
+#include<netinet/if_ether.h>
