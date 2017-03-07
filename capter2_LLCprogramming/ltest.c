6919b250b2dd93e2dd3787730fdfe4cc4b571f60
root <root@localhost.localdomain>
2017-03-07 07:09:12 -0600

    add socketinit function

--- capter2_LLCprogramming/ltest.c
+++ capter2_LLCprogramming/ltest.c
@@ -13,6 +13,19 @@
 #include<netpacket/packet.h>
 #include<netinet/if_ether.h>
 
+int socketinit(char *device,int promiscasmode,int iponly){
+
+//file discriptor is created by socket function
+int sd;
+//when handle linklayer, we use PF_PACKET
+//when handle linklayer, we use SOCK_RAW, other than SOCK_STREAM,SOCK_DGRAM
+sd=socket(PF_PACKET,SOCK_RAW,htons(ETH_P_IP));
+if(sd<0){
+    printf("err!!!");
+}
+return 1;
+}
+
 int main(int argc,char **argv){
     return 0;
 }
