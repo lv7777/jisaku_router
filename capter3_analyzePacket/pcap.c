c9a7f9e74bf4e405d80853a28299d843461c8af8
root <root@localhost.localdomain>
2017-03-08 04:51:41 -0600

    implement basic code

---
+++ capter3_analyzePacket/pcap.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+
+#include<linux/if.h>
+#include<netinet/if_ether.h>
+
+#include<sys/types.h>
+#include<sys/socket.h>
+
+#include<sys/ioctl.h>
+
+#include<net/ethernet.h>
+#include<arpa/inet.h>
+#include<netpacket/packet.h>
+#include<netinet/ip.h>
+
+int main(){
+    
+}
