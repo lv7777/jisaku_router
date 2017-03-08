3c307ff5e735ba692336ae9d6fa17d3abb59edcd
root <root@localhost.localdomain>
2017-03-08 07:34:58 -0600

    implement analyzeArp

--- capter3_analyzePacket/analyze.c
+++ capter3_analyzePacket/analyze.c
@@ -19,3 +19,24 @@
 #ifndef ETHERTYPE_IPV6
 #define ETHERTYPE_IPV6 0x86dd
 #endif
+
+int AnalyzeArp(u_char *data,int size){
+    u_char *ptr;
+    int lest;
+    struct ether_arp *arp;
+    ptr=data;
+    lest=size;
+    if(lest<sizeof(struct ether_arp)){
+        fprintf(stderr,"lest(%d)<sizeof(struct iphdr)\n",lest);
+        return -1;
+    }
+    arp=(struct ether_arp*)ptr;
+
+    //TODO:what this code?
+    ptr+=sizeof(struct ether_arp);
+    lest+=sizeof(struct ether_arp);
+
+    PrintArp(arp,stdout);
+
+    return 0;
+}
