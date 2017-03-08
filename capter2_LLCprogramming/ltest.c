cce2b54c5f64ed7970618aaf06a861dfbe76cd48
root <root@localhost.localdomain>
2017-03-07 19:35:51 -0600

    implement my_ether_ntoa_r function

--- capter2_LLCprogramming/ltest.c
+++ capter2_LLCprogramming/ltest.c
@@ -60,6 +60,11 @@ if(ioctl(sd,SIOCSIFFLAGS,&ifreq)<0){
 return (sd);
 }
 
+char* my_ether_ntoa_r(u_char *hwaddr,char *buf,socketlen_t size){
+    snprintf(buf,size,"%02x:%02x:%02x:%02x:%02x:%02x",hwaddr[0],hwaddr[1],hwaddr[2],hwaddr[3],hwaddr[4],hwaddr[5]);
+    return buf;
+}
+
 int main(int argc,char **argv){
     return 0;
 }
