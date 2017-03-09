3fc8d456b8e7b50baf9d7781085f3d103dc7de34
root <root@localhost.localdomain>
2017-03-09 04:13:26 -0600

    implement arp_ip2str because arp struct is accept u_int8_t, but other structure is accept by u_int32_t

--- capter3_analyzePacket/print.c
+++ capter3_analyzePacket/print.c
@@ -24,3 +24,8 @@ char* my_ether_ntoa_r(u_char *hwaddr,char *buf,socklen_t size){
     snprintf(buf,size,"%02x:%02x:%02x:%02x:%02x:%02x",hwaddr[0],hwaddr[1],hwaddr[2],hwaddr[3],hwaddr[4],hwaddr[5]);
     return buf;
 }
+
+char* arp_ip2str(u_int8_t *ip,char *buf,socket_len_t size){
+    snprintf(buf,size,"%d.%d.%d.%d",ip[0],ip[1],ip[2],ip[3]);
+    return buf;
+}
