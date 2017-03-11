4ee09f62c3ef4b90d4a22ac6bbae52421b63c73e
root <root@localhost.localdomain>
2017-03-10 20:02:42 -0600

    include headers

---
+++ capter3_analyzePacket/checksum.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/ioctl.h>
+#include<sys/socket.h>
+#include<linux.if.h>
+#include<net/ethernet.h>
+#include<arpa/inet.h>
+#include<netpacket/packet.h>
+#include<netinet/if_ether.h>
+#include<netinet/ip.h>
+#include<netinet/ip6.h>
+#include<netinet/ip_icmp.h>
+#include<netinet/icmp6.h>
+#include<netinet/tcp.h>
+#include<netinet/udp.h>
+
