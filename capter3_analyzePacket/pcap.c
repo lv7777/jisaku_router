08da8c9313c9033c0cbaa886a2831afc5dda682a
root <root@localhost.localdomain>
2017-03-15 03:25:33 -0500

    fix builderror part of header

--- capter3_analyzePacket/pcap.c
+++ capter3_analyzePacket/pcap.c
@@ -1,18 +1,13 @@
 #include<stdio.h>
 #include<string.h>
 #include<unistd.h>
-
-#include<linux/if.h>
-#include<netinet/if_ether.h>
-
-#include<sys/types.h>
-#include<sys/socket.h>
-
 #include<sys/ioctl.h>
-
-#include<net/ethernet.h>
 #include<arpa/inet.h>
+#include<sys/socket.h>
+#include<linux/if.h>
+#include<net/ethernet.h>
 #include<netpacket/packet.h>
+#include<netinet/if_ether.h>
 #include<netinet/ip.h>
 #include"analyze.h"
 
