7865a8ed854ccaff2e7cebda604bbeb080f6a66c
root <root@localhost.localdomain>
2017-03-15 00:52:08 -0500

    add checksum2 func

--- capter3_analyzePacket/checksum.c
+++ capter3_analyzePacket/checksum.c
@@ -46,7 +46,7 @@ u_int16_t checksum(u_char *data,int len){
         ptr++;
     }
 
-    
+
     if(c==1){
         u_int16_t val;
         val=0;
@@ -61,3 +61,47 @@ u_int16_t checksum(u_char *data,int len){
     return (~sum);
 }
 
+
+u_int16_t checksum2(u_char *data1, int len1, u_char *data2, int len2) {
+  u_int32_t sum = 0;
+  u_int16_t *ptr;
+  int c;
+  ptr = (u_int16_t *)data1;
+  for (c = len1; c > 1; c -= 2) {
+    sum += (*ptr);
+    if (sum & 0x80000000) {
+      sum = (sum & 0xffff) + (sum >> 16);
+    }
+    ptr++;
+  }
+  if (c == 1) {
+    /* padding */
+    u_int16_t val;
+    val = ((*ptr) << 8) + (*data2);
+    sum += val;
+    if (sum & 0x80000000) {
+      sum = (sum & 0xffff) + (sum >> 16);
+    }
+    ptr = (u_int16_t *)(data2 + 1);
+    len2--;
+  }
+  else {
+    ptr = (u_int16_t *)data2;
+  }
+  for (c = len2; c > 1; c -= 2) {
+    sum += (*ptr);
+    if (sum & 0x80000000) {
+      sum = (sum & 0xffff) + (sum >> 16);
+    }
+    ptr++;
+  }
+  if (c == 1) {
+    u_int16_t val = 0;
+    memcpy(&val, ptr, sizeof(u_int8_t));
+    sum += val;
+  }
+  while (sum >> 16) {
+    sum = (sum & 0xffff) + (sum >> 16);
+  }
+  return ~sum;
+}
