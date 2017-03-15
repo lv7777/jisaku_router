971d9a35a25f619ae3ea7e45bd8de27eb3bdf899
root <root@localhost.localdomain>
2017-03-15 03:32:37 -0500

    create analyze.h

---
+++ capter3_analyzePacket/analyze.h
@@ -0,0 +1,8 @@
+int AnalyzeArp(u_char *,int);
+int AnalyzeIcmp(u_char*,int);
+int AnalyzeIcmp6(u_char*,int);
+int AnalyzeTcp(u_char*,int );
+int AnalyzeUdp(u_char*,int);
+int AnalyzeIp(u_char*,int);
+int AnalyzeIpv6(u_char*,int);
+int AnalyzePacket(u_char*,int);
