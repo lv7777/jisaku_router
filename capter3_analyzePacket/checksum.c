9938bbbbf33816cffcdd3fe0698271164520d2c7
root <root@localhost.localdomain>
2017-03-15 01:27:41 -0500

    implement functions

--- capter3_analyzePacket/checksum.c
+++ capter3_analyzePacket/checksum.c
@@ -105,3 +105,70 @@ u_int16_t checksum2(u_char *data1, int len1, u_char *data2, int len2) {
   }
   return ~sum;
 }
+
+
+int checkIPchecksum(struct iphdr *iphdr,u_char *option,int optionLen)
+{
+unsigned short	sum;
+
+	if(optionLen==0){
+		sum=checksum((u_char *)iphdr,sizeof(struct iphdr));
+		if(sum==0||sum==0xFFFF){
+			return(1);
+		}
+		else{
+			return(0);
+		}
+	}
+	else{
+		sum=checksum2((u_char *)iphdr,sizeof(struct iphdr),option,optionLen);
+		if(sum==0||sum==0xFFFF){
+			return(1);
+		}
+		else{
+			return(0);
+		}
+	}
+}
+
+
+int checkIPDATAchecksum(struct iphdr *iphdr,unsigned char *data,int len)
+{
+struct pseudo_ip        p_ip;
+unsigned short  sum;
+
+        memset(&p_ip,0,sizeof(struct pseudo_ip));
+        p_ip.ip_src.s_addr=iphdr->saddr;
+        p_ip.ip_dst.s_addr=iphdr->daddr;
+        p_ip.ip_p=iphdr->protocol;
+        p_ip.ip_len=htons(len);
+
+        sum=checksum2((unsigned char *)&p_ip,sizeof(struct pseudo_ip),data,len);
+        if(sum==0||sum==0xFFFF){
+                return(1);
+        }
+        else{
+                return(0);
+        }
+}
+
+int checkIP6DATAchecksum(struct ip6_hdr *ip,unsigned char *data,int len)
+{
+struct pseudo_ip6_hdr   p_ip;
+unsigned short  sum;
+
+        memset(&p_ip,0,sizeof(struct pseudo_ip6_hdr));
+
+        memcpy(&p_ip.src,&ip->ip6_src,sizeof(struct in6_addr));
+        memcpy(&p_ip.dst,&ip->ip6_dst,sizeof(struct in6_addr));
+        p_ip.plen=ip->ip6_plen;
+        p_ip.nxt=ip->ip6_nxt;
+
+	sum=checksum2((unsigned char *)&p_ip,sizeof(struct pseudo_ip6_hdr),data,len);
+	if(sum==0||sum==0xFFFF){
+		return(1);
+	}
+	else{
+		return(0);
+	}
+}
